896208a57e1805ea799f89cc47cf341b583dcb17
nekx <nekxala@gmail.com>
2021-02-05 15:00:30 -0600

    Update tampermonkey_script.user.js

--- tampermonkey_script.user.js
+++ tampermonkey_script.user.js
@@ -2,7 +2,7 @@
 // @author Chase Walker
 // @description Allow easy copying of customer data from a given DIFY page
 // @name     Customer info grab
-// @include    *://dify.tigerpistol.com/*/clients/*
+// @include  http*://dify.tigerpistol.com*
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @updateURL https://raw.githubusercontent.com/nekx/dify_customer_info_pull/main/tampermonkey_script.js?token=AD74VBDGU45OA7QIBFGMHSDADWGZC
@@ -384,36 +384,58 @@ const	companies = {
 			"publicName": "Corcoran Social Ad Engine"
 		}
 }
+// select the target node
+var target = document.querySelector('title');
 
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+    // We need only first event and only new value of the title
+    setTimeout(checkLocation, 2000);
+});
 
+// configuration of the observer:
+var config = { subtree: true, characterData: true, childList: true };
 
-waitForKeyElements ("img.account-img", getNodeText);
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+function checkLocation(){
+    if ( "#/clients" === window.location.hash ){
+        console.log(window.location.hash);
+        console.log('failed test');
+        return false;
+    }
+    else if ( window.location.hash.includes("#/clients/") ){
+        console.log(window.location.hash);
+        console.log('passed test');
+        waitForKeyElements ("#single-client-view-business-name", getNodeText);
 
-function getNodeText (jNode) {
-  
-    var clientName = document.getElementById('single-client-view-business-name').innerText;
+        function getNodeText (jNode) {
 
-    var clientID = location.hash.slice(10);
+            var clientName = document.getElementById('single-client-view-business-name').innerText;
 
-    var companyID = localStorage.companyId;
+            var clientID = location.hash.slice(10);
 
-    var companyName = companies[companyID]["name"];
+            var companyID = localStorage.companyId;
 
-    var info = "Client name: " + clientName + ",\nClient ID: " + clientID + ",\nCompany ID: " + companyID + ",\nCompany name: " + companyName;
-  
-  	var clientImg = document.getElementsByClassName("account-img")[0];
-  
-  	clientImg.addEventListener("click", function (event) {
- 
-    	console.log ("Copied to clipboard: ", info);
+            var companyName = companies[companyID]["name"];
 
-      navigator.clipboard.writeText(info).then(
-          function(){
-              alert("Copied: " + info); // success 
-          })
-        .catch(
-           function() {
-              alert("err"); // error
-      });
-    });
-}
+            var info = "Client name: " + clientName + ",\nClient ID: " + clientID + ",\nCompany ID: " + companyID + ",\nCompany name: " + companyName;
+
+            var clientImg = document.getElementsByClassName("account-img")[0];
+
+            clientImg.addEventListener("click", function (event) {
+
+                console.log ("Copied to clipboard: ", info);
+
+                navigator.clipboard.writeText(info).then(
+                    function(){
+                        alert("Copied: " + info); // success
+                    })
+                    .catch(
+                    function() {
+                        alert("err"); // error
+                    });
+            });
+        }
+    };
+};
