3e821b7c5b4f274fac2647c3dd10276c40d9d848
nekx <nekxala@gmail.com>
2021-02-06 00:49:10 -0600

    Update tampermonkey_script.user.js
    Major update

--- tampermonkey_script.user.js
+++ tampermonkey_script.user.js
@@ -9,61 +9,187 @@
 // @updateURL https://github.com/nekx/dify_customer_info_pull/raw/main/tampermonkey_script.user.js
 // @downloadURL https://github.com/nekx/dify_customer_info_pull/raw/main/tampermonkey_script.user.js
 // @grant    GM_setClipboard
-// @version 1.2
+// @grant    GM_addStyle
+// @version 2.0
 // ==/UserScript==
+
+var data = null;
+var social_check = false;
+
+    //--- CSS styles make it work...
+    GM_addStyle ( "                                     \
+#gmPopupContainer {                                     \
+position:               fixed;                          \
+top:                    30%;                            \
+left:                   20%;                            \
+padding:                2em;                            \
+background: -webkit-linear-gradient(top, #0fb2ef 0%, #00a3e0 100%); \
+background: -moz-linear-gradient(top, #0fb2ef 0%, #00a3e0 100%); \
+background: -o-linear-gradient(top, #0fb2ef 0%, #00a3e0 100%); \
+background: -ms-linear-gradient(top, #0fb2ef 0%, #00a3e0 100%); \
+background: linear-gradient(top, #0fb2ef 0%, #00a3e0 100%); \
+border:                 3px double black;               \
+border-radius:          1ex;                            \
+z-index:                777;                            \
+color: white;                                           \
+}                                                       \
+#gmPopupContainer button{                               \
+cursor:                 pointer;                        \
+margin:                 1em 1em 0;                      \
+border:                 1px outset buttonface;          \
+background: white;                                      \
+color: black;                                           \
+}                                                       \
+#gmPopupContainer label{                                \
+display:                inline;                         \
+}                                                       \
+" );
+
+function checkLocation(){
+
+
+    if ( "#/clients" === window.location.hash ){
+        return false;
+    }
+    else if ( window.location.hash.includes("#/clients/") ){
+        waitForKeyElements ("#single-client-view-business-name", gatherData);
+
+        function gatherData () {
+            social_check = false;
+            data = {
+                "clientName" : document.getElementById('single-client-view-business-name').innerText,
+                "clientID" : null,
+                "companyID" : localStorage.companyId,
+                "companyName" : null,
+                "facebook_page_ID" : null,
+                "ad_account_ID" : null
+            };
+            data["companyName"] = companies[data["companyID"]]["name"];
+
+         	if (window.location.hash.split("/").pop() === "social"){
+                waitForKeyElements ("a.partner-color", fbDataGather);
+                social_check = true;
+            }
+			else{
+                waitForKeyElements ('div[data-bind="click: $component.showCampaign.bind($component), css: $component.getActiveRowCss(ko.unwrap(id))"]', campaignCopy);
+            }
+            waitForKeyElements ('.account-img', addEventListeners)
+        }
+    }
+  }
+function addEventListeners(){
+    var clientImg = document.getElementsByClassName("account-img")[0];
+
+    clientImg.addEventListener("contextmenu", function(e){e.preventDefault();e.stopImmediatePropagation();copyPopup();return false;}, false);
+    clientImg.addEventListener("click", function(evt){
+        gatherCopy();
+        evt.stopImmediatePropagation();
+        return false;
+    });
+}
+
+function fbDataGather (){
+    data["clientID"] = window.location.hash.split("/").slice(-2, -1).toString();
+    data["facebook_page_ID"] = $("a.line-height-pic").attr('href').split("/").pop();
+	data["ad_account_ID"] = $("a.partner-color").attr('href').split("=").pop();
+}
+
+function campaignCopy (){
+    data["clientID"] = window.location.hash.split("/").slice(-1).toString();
+    var campaign_names =  $('span[data-bind="text: $data.name(), maxLength: 30"]');
+
+    var campaign_IDs = $('a:hidden[data-bind*="campaigns/"]').map(function(i,el) { return $(el).attr('href').split("/").slice(-2, -1).toString(); }).get();
+
+    $.each(campaign_names, function( index ) {
+        $(this).click(function(){gatherCopy(campaign_IDs[index])})
+        $(this).attr('title', campaign_IDs[index]);
+    });
+    return false;
+}
+
+function copyPopup(){
+    $("#gmPopupContainer").toggle();
+    if (! social_check){
+        $("#facebook_page_ID").css("display", "none")
+        $("#ad_account_ID").css("display", "none")
+        $('label[for="facebook_page_ID"]').css("display", "none")
+        $('label[for="ad_account_ID"]').css("display", "none")
+    };
+
+
+    //--- Use jQuery to activate the dialog buttons.
+    $("#gmCopyButton").click ( function () {
+        gatherCopy(true);
+    } );
+
+    $("#gmCloseButton").click ( function (evt) {
+        $("#gmPopupContainer").toggle();
+        evt.stopImmediatePropagation()
+    } );
+return false;
+};
+
+function gatherCopy(popup=false){
+    var info = "";
+    var inputs;
+    if (popup){ inputs = $("input[type='checkbox']:checked"); }
+    else { inputs = $("input[type='checkbox']"); }
+    inputs.each ( function (index) {
+        var id = $(this).attr("id");
+        var value = data[id]
+        if (value != null){
+            if (index == 0){
+                info = info.concat($(this).val() + ": " + value);
+            }
+            else {
+                info = info.concat(",\n" + $(this).val() + ": " + value);
+            }
+        }
+    });
+    clipboardCopy(info);
+    return false;
+};
+
+function clipboardCopy(copy){
+    console.log ("Copied to clipboard: ", copy);
+
+    navigator.clipboard.writeText(copy).then(
+        function(){
+            alert("Copied: \n" + copy); // success
+        })
+        .catch(
+        function() {
+            alert("err"); // error
+        });
+    return false;
+}
 
 // checks for changes to the title, waits 2 seconds and runs checkLocation()
 var target = document.querySelector('title');
 
 var observer = new MutationObserver(function(mutations) {
-    setTimeout(checkLocation, 2000);
+    $("#gmPopupContainer").remove();
+    $("body").append ( '                                                          \
+<div id="gmPopupContainer" style="display:none;">                                               \
+<input type="checkbox" id="clientName" value="Client Name">                      \
+<label for="clientName">Client Name</label><br>                              \
+<input type="checkbox" id="clientID" value="Client ID">                        \
+<label for="clientID">Client ID</label><br>                              \
+<input type="checkbox" id="companyID" value="Company ID">                       \
+<label for="companyID">Company ID</label><br>                             \
+<input type="checkbox" id="companyName" value="Company Name">                     \
+<label for="companyName">Company Name</label><br>                              \
+<input type="checkbox" id="facebook_page_ID" value="Facebook Page ID">                \
+<label for="facebook_page_ID">Facebook Page ID</label><br>                              \
+<input type="checkbox" id="ad_account_ID" value="Ad account ID">                   \
+<label for="ad_account_ID">Ad account ID</label><br>                             \
+<button id="gmCopyButton" type="button">Copy selected values</button>  \
+<button id="gmCloseButton" type="button">Close popup</button>         \
+</div>                                                                    \
+' );
+    setTimeout(checkLocation, 3000);
 });
 
 var config = { subtree: true, characterData: true, childList: true };
 
 observer.observe(target, config);
-
-function checkLocation(){
-    if ( "#/clients" === window.location.hash ){
-        return false;
-    }
-    else if ( window.location.hash.includes("#/clients/") ){
-        waitForKeyElements ("#single-client-view-business-name", getNodeText);
-
-        function getNodeText (jNode) {
-          
-            var clientName = document.getElementById('single-client-view-business-name').innerText;
-
-            var clientID =  window.location.hash.split("/").slice(-2, -1).toString()
-
-            var companyID = localStorage.companyId;
-
-            var companyName = companies[companyID]["name"];
-          
-         	if (window.location.hash.split("/").pop() === "social"){
-              	var facebook_page_ID = $("a.line-height-pic").attr('href').split("/").pop();
-				var ad_account_ID = $("a.partner-color").attr('href').split("=").pop(); 
-              	var info = "Client name: " + clientName + ",\nClient ID: " + clientID + ",\nCompany ID: " + companyID + ",\nCompany name: " + companyName + ",\nFacebook Page ID: " + facebook_page_ID + ",\nAd Account ID: " + ad_account_ID;
-            }
-			else{
-            	var info = "Client name: " + clientName + ",\nClient ID: " + clientID + ",\nCompany ID: " + companyID + ",\nCompany name: " + companyName;
-            };
-          
-            var clientImg = document.getElementsByClassName("account-img")[0];
-
-            clientImg.addEventListener("click", function (event) {
-
-                console.log ("Copied to clipboard: ", info);
-
-                navigator.clipboard.writeText(info).then(
-                    function(){
-                        alert("Copied: " + info); // success
-                    })
-                    .catch(
-                    function() {
-                        alert("err"); // error
-                    });
-            });
-        }
-    };
-};
