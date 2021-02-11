478354eb09c0f2fbb2749ac3e712b8ea5bc79953
nekx <nekxala@gmail.com>
2021-02-11 13:35:08 -0600

    Update tampermonkey_script.user.js
    forgot to update version #

--- tampermonkey_script.user.js
+++ tampermonkey_script.user.js
@@ -13,7 +13,7 @@
 // @grant    GM_setClipboard
 // @grant    GM_addStyle
 // @grant    GM_getResourceText
-// @version 2.1
+// @version 2.2
 // ==/UserScript==
 
 var data = null;                                         // contains all copy-able data for the popup
