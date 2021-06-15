15dfd855f0d91db1523f69b8d1b19c94a268a0d7
nekx <nekxala@gmail.com>
2021-06-15 14:27:29 -0500

    update for last fix

--- constants.js
+++ constants.js
@@ -1,471 +1,377 @@
 const partners =  {
-	{
-		"id": "44",
+"44":{
 		"name": "Tiger Pistol Demo Subscriptions",
 		"publicName": "Tiger Pistol Demo",
 	},
-	{
-		"id": "227",
+"227":{
 		"name": "ABI Belgium ",
 		"publicName": "AB InBev Belgium",
 	},
-	{
-		"id": "335",
+"335":{
 		"name": "ABI Bolivia",
 		"publicName": "Espacio Cervecero",
 	},
-	{
-		"id": "265",
+"265":{
 		"name": "ABI Brazil - Ambev Regional MG",
 		"publicName": "Ambev Regional MG",
 	},
-	{
-		"id": "195",
+"195":{
 		"name": "ABI Mexico - Grupo Modelo",
 		"publicName": "Publicidad Social Grupo Modelo",
 	},
-	{
-		"id": "97",
+"97":{
 		"name": "zzz_AdMax Local - BloomNation ",
 		"publicName": "zzz_BloomNation Social",
 	},
-	{
-		"id": "120",
+"120":{
 		"name": "zzz_AdMax Local - Haines Local Search",
 		"publicName": "zzz_Haines Local Search",
 	},
-	{
-		"id": "205",
+"205":{
 		"name": "zzz_AdMax Local - Kitchen United",
 		"publicName": "zzz_Kitchen United",
 	},
-	{
-		"id": "204",
+"204":{
 		"name": "zzz_AdMax Local - Testing",
 		"publicName": "zzz_AdMax Local - Testing",
 	},
-	{
-		"id": "191",
+"191":{
 		"name": "zzz_AdMax Local - Urban Air",
 		"publicName": "zzz_Urban Air",
 	},
-	{
-		"id": "143",
+"143":{
 		"name": "zzz_AdMax Local Retail",
 		"publicName": "zzz_AdMax Local",
 	},
-	{
-		"id": "212",
+"212":{
 		"name": "Better Homes and Gardens Real Estate",
 		"publicName": "Better Homes and Gardens Real Estate Social Ad Engine",
 	},
-	{
-		"id": "192",
+"192":{
 		"name": "BrandMuscle - Allstate",
 		"publicName": "BrandMuscle",
 	},
-	{
-		"id": "108",
+"108":{
 		"name": "BrandMuscle - AmFam ",
 		"publicName": "BrandMuscle",
 	},
-	{
-		"id": "200",
+"200":{
 		"name": "ZZZ_BrandMuscle - ATT",
 		"publicName": "ZZZ_BrandMuscle - ATT",
 	},
-	{
-		"id": "294",
+"294":{
 		"name": "BrandMuscle - Bobcat",
 		"publicName": "BrandMuscle - Bobcat",
 	},
-	{
-		"id": "202",
+"202":{
 		"name": "BrandMuscle - USC",
 		"publicName": "BrandMuscle - USC",
 	},
-	{
-		"id": "220",
+"220":{
 		"name": "Canada - Sotheby's International Realty",
 		"publicName": "Sotheby’s International Realty Social Ad Engine",
 	},
-	{
-		"id": "213",
+"213":{
 		"name": "CENTURY 21",
 		"publicName": "CENTURY 21 Social Ad Engine",
 	},
-	{
-		"id": "209",
+"209":{
 		"name": "Coldwell Banker Real Estate",
 		"publicName": "Coldwell Banker Real Estate Social Ad Engine",
 	},
-	{
-		"id": "316",
+"316":{
 		"name": "Corcoran Group - Test",
 		"publicName": "Corcoran Group - Test",
 	},
-	{
-		"id": "116",
+"116":{
 		"name": "CSERV-Demo",
 		"publicName": "CSERV-Demo Social",
 	},
-	{
-		"id": "242",
+"242":{
 		"name": "Demo - Beverage Brands",
 		"publicName": "BevCo",
 	},
-	{
-		"id": "175",
+"175":{
 		"name": "Digital Pharmacist",
 		"publicName": "Digital Pharmacist",
 	},
-	{
-		"id": "39",
+"39":{
 		"name": "Entravision",
 		"publicName": "Entravision",
 	},
-	{
-		"id": "211",
+"211":{
 		"name": "ERA",
 		"publicName": "ERA Social Ad Engine",
 	},
-	{
-		"id": "286",
+"286":{
 		"name": "Geometry Global CAD",
 		"publicName": "Geometry Social",
 	},
-	{
-		"id": "258",
+"258":{
 		"name": "Geometry Global USD",
 		"publicName": "Geometry Social",
 	},
-	{
-		"id": "197",
+"197":{
 		"name": "GoDaddy Social",
 		"publicName": "GoDaddy Social",
 	},
-	{
-		"id": "12",
+"12":{
 		"name": "Hibu Display",
 		"publicName": "Hibu",
 	},
-	{
-		"id": "33",
+"33":{
 		"name": "Hibu Social",
 		"publicName": "Hibu",
 	},
-	{
-		"id": "7",
+"7":{
 		"name": "hibu Social",
 		"publicName": "Hibu",
 	},
-	{
-		"id": "102",
+"102":{
 		"name": "January Spring",
 		"publicName": "Christian Terry",
 	},
-	{
-		"id": "230",
+"230":{
 		"name": "LocalEdge",
 		"publicName": " Your Digital Support Team, Social Media",
 	},
-	{
-		"id": "174",
+"174":{
 		"name": "Localsearch",
 		"publicName": "Localsearch",
 	},
-	{
-		"id": "215",
+"215":{
 		"name": "NRT - Coldwell Banker Residential",
 		"publicName": "Coldwell Banker Social Ad Engine",
 	},
-	{
-		"id": "214",
+"214":{
 		"name": "NRT - Sotheby's International Realty",
 		"publicName": "Sotheby's International Realty Social Ad Engine",
 	},
-	{
-		"id": "170",
+"170":{
 		"name": "OutboundEngine",
 		"publicName": "OutboundEngine",
 	},
-	{
-		"id": "52",
+"52":{
 		"name": "PaperBrix",
 		"publicName": "PaperBrix",
 	},
-	{
-		"id": "146",
+"146":{
 		"name": "Paperbrix - Forte Digital GroupId: 146",
 		"publicName": "Forte Forward",
 	},
-	{
-		"id": "134",
+"134":{
 		"name": "Qiigo USD",
 		"publicName": "Qiigo",
 	},
-	{
-		"id": "208",
+"208":{
 		"name": "Sotheby's International Realty",
 		"publicName": "Sotheby's International Realty Social Ad Engine",
 	},
-	{
-		"id": "193",
+"193":{
 		"name": "Spotzer - Telstra",
 		"publicName": "Telstra Business Digital Marketing Services",
 	},
-	{
-		"id": "15",
+"15":{
 		"name": "Test Partner",
 		"publicName": "Test Partner",
 	},
-	{
-		"id": "157",
+"157":{
 		"name": "The Galley",
 		"publicName": "The Galley",
 	},
-	{
-		"id": "252",
+"252":{
 		"name": "[New] BrandMuscle - Allstate",
 		"publicName": "BrandMuscle",
 	},
-	{
-		"id": "249",
+"249":{
 		"name": "[New] BrandMuscle - AmFam",
 		"publicName": "BrandMuscle",
 	},
-	{
-		"id": "254",
+"254":{
 		"name": "BrandMuscle - ATT",
 		"publicName": "BrandMuscle - ATT",
 	},
-	{
-		"id": "251",
+"251":{
 		"name": "[New] BrandMuscle - Farmers Insurance",
 		"publicName": "BrandMuscle - Farmers Insurance",
 	},
-	{
-		"id": "253",
+"253":{
 		"name": "[New] BrandMuscle - USC",
 		"publicName": "BrandMuscle - USC",
 	},
-	{
-		"id": "334",
+"334":{
 		"name": "Westfield Agency Social Advertising Program",
 		"publicName": "Westfield Agency Social Advertising Program",
 	},
-	{
-		"id": "299",
+"299":{
 		"name": "Rodan + Fields Pilot",
 		"publicName": "R + F Local Paid Media Pilot",
 	},
-	{
-		"id": "305",
+"305":{
 		"name": "Mutual of Omaha",
 		"publicName": "Social Lead Accelerator",
 	},
-	{
-		"id": "279",
+"279":{
 		"name": "ABI - Canada AMPLIFY ",
 		"publicName": "AMPLIFY by Labatt",
 	},
-	{
-		"id": "307",
+"307":{
 		"name": "ABI Austria",
 		"publicName": "ABI Geotargeted Social Ads",
 	},
-	{
-		"id": "288",
+"288":{
 		"name": "ABI Brazil - Ambev AC",
 		"publicName": "AmbevAC",
 	},
-	{
-		"id": "267",
+"267":{
 		"name": "ABI Brazil - Ambev Regional CO",
 		"publicName": "Ambev Regional CO",
 	},
-	{
-		"id": "269",
+"269":{
 		"name": "ABI Brazil - Ambev Regional NE",
 		"publicName": "Ambev NE",
 	},
-	{
-		"id": "271",
+"271":{
 		"name": "ABI Brazil - Ambev Regional NO",
 		"publicName": "Ambev Regional NO",
 	},
-	{
-		"id": "263",
+"263":{
 		"name": "ABI Brazil - Ambev Regional RJ ES",
 		"publicName": "Ambev Regional RJ ES",
 	},
-	{
-		"id": "261",
+"261":{
 		"name": "ABI Brazil - Ambev Regional SP",
 		"publicName": "Ambev Regional SP",
 	},
-	{
-		"id": "273",
+"273":{
 		"name": "ABI Brazil - Ambev Regional Sul",
 		"publicName": "Ambev Regional Sul",
 	},
-	{
-		"id": "340",
+"340":{
 		"name": "ABI Colombia",
 		"publicName": "MenúDig Publicidad",
 	},
-	{
-		"id": "342",
+"342":{
 		"name": "ABI Colombia (BEES)",
 		"publicName": "BEES - Colombia",
 	},
-	{
-		"id": "337",
+"337":{
 		"name": "ABI Dominican Republic (BEES)",
 		"publicName": "BEES",
 	},
-	{
-		"id": "292",
+"292":{
 		"name": "ABI Italy ",
 		"publicName": "ABI Italia",
 	},
-	{
-		"id": "235",
+"235":{
 		"name": "ABI NL - Bud Social Euros",
 		"publicName": "Bud Social",
 	},
-	{
-		"id": "237",
+"237":{
 		"name": "ABI South Africa - Sisonke",
 		"publicName": "Sisonke",
 	},
-	{
-		"id": "233",
+"233":{
 		"name": "ABI UK - BBG",
 		"publicName": "Budweiser Brewing Group",
 	},
-	{
-		"id": "331",
+"331":{
 		"name": "Avery Healthcare",
 		"publicName": "Avery Healthcare",
 	},
-	{
-		"id": "333",
+"333":{
 		"name": "BuzzBallz",
 		"publicName": "BuzzBallz",
 	},
-	{
-		"id": "232",
+"232":{
 		"name": "Forty Winks",
 		"publicName": "Forty Winks",
 	},
-	{
-		"id": "291",
+"291":{
 		"name": "JOANN",
 		"publicName": "JOANN",
 	},
-	{
-		"id": "257",
+"257":{
 		"name": "L'Oreal US",
 		"publicName": "Back In Style Consumer Advertising",
 	},
-	{
-		"id": "343",
+"343":{
 		"name": "Customer Engagement Sandbox",
 		"publicName": "Customer Engagement Sandbox",
 	},
-	{
-		"id": "336",
+"336":{
 		"name": "GoDaddy Social Two-Tier",
 		"publicName": "GoDaddy",
 	},
-	{
-		"id": "300",
+"300":{
 		"name": "AffinityX - Metroland",
 		"publicName": "My Social Ads",
 	},
-	{
-		"id": "180",
+"180":{
 		"name": "8homes Urbanedge (Mahercorp)",
 		"publicName": "8homes Urbanedge",
 	},
-	{
-		"id": "308",
+"308":{
 		"name": "ABI Switzerland",
 		"publicName": "ABI Geotargeted Social Ads",
 	},
-	{
-		"id": "78",
+"78":{
 		"name": "Asahi - Retail",
 		"publicName": "Asahi Premium Beverages",
 	},
-	{
-		"id": "55",
+"55":{
 		"name": "Asahi - Third Space Platform",
 		"publicName": "Asahi Premium Beverages",
 	},
-	{
-		"id": "228",
+"228":{
 		"name": "LMG (Liquor Marketing Group)",
 		"publicName": "LMG (Liquor Marketing Group)",
 	},
-	{
-		"id": "87",
+"87":{
 		"name": "ZZZ_McGrath",
 		"publicName": "ZZZ_McGrath Social Advertising Program",
 	},
-	{
-		"id": "236",
+"236":{
 		"name": "Schumacher Homes",
 		"publicName": "Schumacher Homes",
 	},
-	{
-		"id": "239",
+"239":{
 		"name": "Zambrero Social Ads Tool",
 		"publicName": "Zambrero Social Ads Tool",
 	},
-	{
-		"id": "94",
+"94":{
 		"name": "Yellow Social Ads (Sensis/Yellow Pages) ",
 		"publicName": "Yellow Social Ads",
 	},
-	{
-		"id": "346",
+"346":{
 		"name": "Qiigo CAD",
 		"publicName": "Qiigo",
 	},
-	{
-		"id": "319",
+"319":{
 		"name": "The Corcoran Group",
 		"publicName": "Corcoran Social Ad Engine",
 	},
-	{
-		"id": "356",
+"356":{
 		"name": "Lincoln Investment",
 		"publicName": "Lincoln Investment Social Ads Pilot",
 	},
-	{
-		"id": "361",
+"361":{
 		"name": "Great Clips",
 		"publicName": "Great Clips Social Ads",
 	},
-	{
-		"id": "360",
+"360":{
 		"name": "ABI Brazil BEES",
 		"publicName": "ABI Brazil BEES",
 	},
-	{
-		"id": "369",
+"369":{
 		"name": "ABI Argentina BEES",
 		"publicName": "ABI Argentina BEES",
 	},
-	{
-		"id": "359",
+"359":{
 		"name": "ABI Ecuador BEES",
 		"publicName": "ABI Ecuador BEES",
 	},
-	{
-		"id": "357",
+"357":{
 		"name": "ABI Mexico BEES",
 		"publicName": "ABI Mexico BEES",
 	}
