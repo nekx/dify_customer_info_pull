8b124b04f5650969f17421d75033b1ef7f2c90d3
nekx <nekxala@gmail.com>
2021-08-09 13:52:48 -0500

    campaign copy fix
    removed unused functions / tampermonkey values from previous version. Changed campaign copy to check the same element that the native page JS changes, and updated the styling

--- tampermonkey_script.user.js
+++ tampermonkey_script.user.js
@@ -4,28 +4,12 @@
 // @name     Customer info grab
 // @include  http*://dify.tigerpistol.com/*
 // @noframes
-// @resource htmlTemplate https://raw.githubusercontent.com/nekx/dify_customer_info_pull/main/selection.html
-// @resource cssTemplate https://raw.githubusercontent.com/nekx/dify_customer_info_pull/main/selection.css
-// @resource accountImgCSSTemplate https://raw.githubusercontent.com/nekx/dify_customer_info_pull/main/accountImg.css
 // @updateURL https://github.com/nekx/dify_customer_info_pull/raw/main/tampermonkey_script.user.js
 // @downloadURL https://github.com/nekx/dify_customer_info_pull/raw/main/tampermonkey_script.user.js
 // @grant    GM_setClipboard
-// @grant    GM_addStyle
-// @grant    GM_getResourceText
-// @grant    GM_openInTab
-// @grant    GM_setValue
-// @grant    GM_getValue
-// @version 4.0.5
+// @version 4.0.6
 // ==/UserScript==
 
-
-var data = null;                                         // contains all copy-able data for the popup
-var social_check = false;                                // Marker for the social page
-const cssTemplate = GM_getResourceText ("cssTemplate");    // css template
-const htmlTemplate = GM_getResourceText ("htmlTemplate");  // html template
-const accountImgTemplate = GM_getResourceText ("accountImgCSSTemplate");  // html template
-
-GM_addStyle ( cssTemplate );                             // applies cssTemplate
 pageInfo = []
 campaignList = []
   
@@ -34,99 +18,88 @@ XMLHttpRequest.prototype.open = function() {
 
     this.addEventListener('load', function() {
 
-        var target = document.getElementsByClassName('title')[0]
-        
-        if (this.responseURL.includes("/api/clients/") && !this.responseURL.includes('/onboarding/ipc')){
-        
-//            console.log(this)
-            parsedResponse = JSON.parse(this.responseText)
-            pageInfo.businessName = ['DIFY Name', parsedResponse.name]
-            pageInfo.businessID = ['DIFY ID', parsedResponse.id]
-            pageInfo.companyID = ['DIFY Instance ID', parsedResponse.companyId]
-            if(pageInfo.companyList){
-                pageInfo.companyName = ['DIFY Instance Name', pageInfo.companyList.find((element) => element.id == pageInfo.companyID[1]).name]
-                delete pageInfo.companyList
+        let pageCheck = window.location.href.includes('clients/')
+        if (pageCheck){
+            if (this.responseURL.includes("/api/clients/") && !this.responseURL.includes('/onboarding/ipc')){
+            
+    //            console.log(this)
+                parsedResponse = JSON.parse(this.responseText)
+                pageInfo.businessName = ['DIFY Name', parsedResponse.name]
+                pageInfo.businessID = ['DIFY ID', parsedResponse.id]
+                pageInfo.companyID = ['DIFY Instance ID', parsedResponse.companyId]
+                if(pageInfo.companyList){
+                    pageInfo.companyName = ['DIFY Instance Name', pageInfo.companyList.find((element) => element.id == pageInfo.companyID[1]).name]
+                    delete pageInfo.companyList
+                }
+                try{
+                    pageInfo.facebookPageID = ['FB Page ID', parsedResponse.socialAccounts[0].id]
+                }
+                catch(e){
+                    console.log('Couldn\'t find FB ID')
+                }
+                try{
+                pageInfo.facebookAdID = ['FB Ad ID', parsedResponse.facebookAdAccountId.slice(4)]
+                }
+                catch(e){
+                    console.log('Couldn\'t find FB ad ID')
+                }
+                var clientName = document.getElementsByClassName('title')[0]
+                addEvent(clientName, "click", function(e){
+                    e.preventDefault();
+                    copyData(pageInfo);
+                    return false;
+                })
             }
-            try{
-                pageInfo.facebookPageID = ['FB Page ID', parsedResponse.socialAccounts[0].id]
+            else if(this.responseURL.includes("currentUser")){
+                pageInfo.companyList = JSON.parse(this.responseText).companies
+                if (pageInfo.companyID){
+                    pageInfo.companyName = ['DIFY Instance Name',pageInfo.companyList.find((element) => element.id == pageInfo.companyID[1]).name]
+                    delete pageInfo.companyList
+                }
             }
-            catch(e){
-                console.log('Couldn\'t find FB ID')
+            else if(this.responseURL.includes("campaigns?")){
+                results = JSON.parse(this.responseText)['results']
+                for (result in results){
+                    result = results[result]
+                    if (campaignList.findIndex(x => x[0]==result['id']) == -1){
+                        campaignList.push([result['id'], result['name']])
+                    }
+                }
             }
-            try{
-            pageInfo.facebookAdID = ['FB Ad ID', parsedResponse.facebookAdAccountId.slice(4)]
-            }
-            catch(e){
-                console.log('Couldn\'t find FB ad ID')
-            }
-            doTheThing(target)
-        }
-        else if(this.responseURL.includes("currentUser")){
-            pageInfo.companyList = JSON.parse(this.responseText).companies
-            if (pageInfo.companyID){
-                pageInfo.companyName = ['DIFY Instance Name',pageInfo.companyList.find((element) => element.id == pageInfo.companyID[1]).name]
-                delete pageInfo.companyList
-            }
-        }
-        else if(this.responseURL.includes("campaigns?")){
-            results = JSON.parse(this.responseText)['results']
-            for (result in results){
-                result = results[result]
-                campaignList.push([result['id'], result['name']])
-            }
-        }
-        else if(this.responseURL.includes("activeCopy=true")){
-            var observer = new MutationObserver(function(mutations){
-                for (let mutation of mutations) {
-                    let addedNode = mutation.addedNodes[0]
-                    try{
-                        if (addedNode.href){
-                            campaignID = addedNode.href.slice(39, -6)
-                            if (campaignID == campaignList[campaignList.length-1][0]){
-                                campaignCopy()
+            else if((this.responseURL.includes("activeCopy=true") || this.responseURL.includes("facebook/picture"))){
+                var campaignObserver = new MutationObserver(function(mutations){
+                    for (let mutation of mutations) {
+                        let addedNode = mutation.addedNodes[0]
+                        try{
+                            if (addedNode.href){
+                                campaignID = addedNode.href.slice(39, -6)
+                                if (campaignID == campaignList[campaignList.length-1][0]){
+                                    campaignCopy()
+                                }
+                            }
+                        }
+                        catch(e){
+                            if (e.name != 'TypeError'){
+                                throw(e)
                             }
                         }
                     }
-                    catch(e){
-                        if (e.name != 'TypeError'){
-                            throw(e)
-                        }
-                    }
-                }
-            })
-            var options = {subtree: true, childList: true}
-            observer.observe(document.getElementsByClassName('campaigns-table')[0], options)
+                })
+               var options = {subtree: true, childList: true}
+//                campaignObserver.observe(document.getElementsByClassName('campaigns-table')[0], options)
+                campaignObserver.observe(document.querySelector('[id^="ko-component"]'), options)
 
+            }
+        }   
+        else{
+            pageInfo = []
+            campaignList = []
         }
-        
     });
 
     origOpen.apply(this, arguments);
 };
 
-function doTheThing(target){
-    
-    // client name node
-    const clientName = target
-
-    // sets right click to copyPopup()
-    clientName.addEventListener("contextmenu", function(e){
-        e.preventDefault();
-        gatherData();
-        copyPopup();
-        return false;
-    })
-
-    clientName.addEventListener("click", function(e){
-        e.preventDefault();
-        copyData(pageInfo);
-        return false;
-    })
-}
-
-function gatherData(){
-    console.log('gatherData()')
-}
 
 function copyData(){
     data = []
@@ -151,53 +124,6 @@ function clipboardCopy(copy){
         });
     return false;
 }
-// creates the popup modal and it's input / buttons
-function copyPopup(){
-    
-    console.log('copyPopup()')
-    let self = this
-
-    let popupContainer = document.getElementById('gmPopupContainer')
-    let body = document.getElementsByTagName('body')[0]
-    
-    // removes the popup
-    if (document.contains(popupContainer)){
-        popupContainer.remove()
-    }
-    // refreshes the htmlTemplate by appending it
-    body.insertAdjacentHTML('afterend', htmlTemplate)
-    popupContainer = document.getElementById('gmPopupContainer')
-    let templateButton = document.getElementById('gmTemplateButton')
-    let copyNoTitleButton = document.getElementById('gmCopyNoTitleButton')
-    let copyButton = document.getElementById("gmCopyButton")
-    let closeButton = document.getElementById("gmCloseButton")
-  
-    // display the popup
-    popupContainer.style.display = "block"
-
-    // sets left click of the template button
-    templateButton.addEventListener("click", function(e){
-
-    })
-
-    // set the left click of the No Titles button to gatherCopy() with popup=true and copyTitles=false
-    copyNoTitleButton.addEventListener("click", function(e){
-
-    })
-
-    // sets the left click of the Copy button to gatherCopy with popup=true
-    copyButton.addEventListener("click", function(e){
-
-    })
-
-    // sets the left click of Close button to close the popup
-    closeButton.addEventListener("click", function(e){
-        popupContainer.classList.toggle('visable');
-        e.stopImmediatePropagation();
-    })
-
-return false;
-}
 
 function campaignCopy(){
     targetList = []
@@ -211,10 +137,10 @@ function campaignCopy(){
         let campaignNameSpan = campaignNameContainer.childNodes[0]
         let flipIcon = container.childNodes[1].childNodes[1]
         targetList.push([campaignNameSpan, campaignID, campaignName])
-        flipIcon.addEventListener("click", function(e){
+        addEvent(flipIcon, "click", function(e){
             campaignFlip(campaignNameSpan, campaignID, campaignName)
         })
-        campaignNameContainer.addEventListener("click", function(e){
+        addEvent(campaignNameContainer, "click", function(e){
             if (e.shiftKey){
                 clipboardCopy(campaignID + ' : ' + campaignName)
             }
@@ -222,18 +148,18 @@ function campaignCopy(){
                 clipboardCopy(campaignID)
             }
         })
-        campaignNameContainer.addEventListener("contextmenu", function(e){
+        addEvent(campaignNameContainer, "contextmenu", function(e){
             clipboardCopy(campaignName)
         })
-        campaignNameContainer.addEventListener("auxclick", function(e){
-            console.log(e.button)
+        addEvent(campaignNameContainer, 'auxclick', function(e){
             if (e.button == 1) {
                     e.preventDefault();
                 }
         })
     }
     allClickTarget = document.getElementsByClassName('fa-pie-chart')[0]
-    allClickTarget.addEventListener("click", function(e){
+    addEvent(allClickTarget, "click", function(e){
+        e.preventDefault
         for (var x = 0; x <= targetList.length-1; x++){
             campaignFlip(targetList[x][0], targetList[x][1], targetList[x][2])
         }
@@ -242,6 +168,7 @@ function campaignCopy(){
 }
 
 function campaignFlip(target, campaignID, campaignName){
+    parentDiv = target.parentElement.parentElement
     if (target.innerText == campaignID){
         target.innerText = campaignName
     }
@@ -249,4 +176,14 @@ function campaignFlip(target, campaignID, campaignName){
         target.innerText = campaignID
     }
     target.setAttribute("style", "white-space:normal!important")
+    parentDiv.setAttribute("style", "height: fit-content!important")
+}
+
+function addEvent(target, type,  func){
+
+    if (!target.hasAttribute('added')){
+        target.addEventListener(type, func)
+        target.setAttribute('added', '')
+    }
+
 }
